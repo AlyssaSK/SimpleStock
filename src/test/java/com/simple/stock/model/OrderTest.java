8eb416f5da32f18b850adba2f7e249f3761da700
chervotkinaaa <ayman.00@mail.ru>
2017-03-20 14:37:11 +0300

    * renamed Client -> Customer

--- src/test/java/com/simple/stock/model/OrderTest.java
+++ src/test/java/com/simple/stock/model/OrderTest.java
@@ -12,54 +12,54 @@ public class OrderTest {
     @Test
     public void getOrderFromString() throws Exception {
         String testString = "C1\ts\tA\t15\t4";
-        Order order = new Order( new Client("C1"), OperationType.SELL, ShareType.A,15,4);
+        Order order = new Order( new Customer("C1"), OperationType.SELL, ShareType.A,15,4);
         assertEquals(order, Order.getOrderFromString(testString));
     }
 
     @Test
     public void isCorresponds() throws Exception {
-        Order orderC1 = new Order( new Client("C1"), OperationType.SELL, ShareType.A,15,4);
-        Order orderC2 = new Order( new Client("C2"), OperationType.BUY,  ShareType.A,15,4);
+        Order orderC1 = new Order( new Customer("C1"), OperationType.SELL, ShareType.A,15,4);
+        Order orderC2 = new Order( new Customer("C2"), OperationType.BUY,  ShareType.A,15,4);
 
         assertTrue(orderC1.isCorresponds(orderC2));
     }
 
     @Test
     public void isNotCorrespondsOneOperation() throws Exception {
-        Order orderC1 = new Order( new Client("C1"), OperationType.SELL, ShareType.A,15,4);
-        Order orderC2 = new Order( new Client("C2"), OperationType.SELL,  ShareType.A,15,4);
+        Order orderC1 = new Order( new Customer("C1"), OperationType.SELL, ShareType.A,15,4);
+        Order orderC2 = new Order( new Customer("C2"), OperationType.SELL,  ShareType.A,15,4);
 
         assertFalse(orderC1.isCorresponds(orderC2));
     }
 
     @Test
     public void isNotCorrespondsOneClient() throws Exception {
-        Order orderC1 = new Order( new Client("C1"), OperationType.SELL, ShareType.A,15,4);
-        Order orderC2 = new Order( new Client("C1"), OperationType.BUY,  ShareType.A,15,4);
+        Order orderC1 = new Order( new Customer("C1"), OperationType.SELL, ShareType.A,15,4);
+        Order orderC2 = new Order( new Customer("C1"), OperationType.BUY,  ShareType.A,15,4);
 
         assertFalse(orderC1.isCorresponds(orderC2));
     }
 
     @Test
     public void isNotCorrespondsDifferentShareType() throws Exception {
-        Order orderC1 = new Order( new Client("C1"), OperationType.SELL, ShareType.A,15,4);
-        Order orderC2 = new Order( new Client("C2"), OperationType.BUY,  ShareType.C,15,4);
+        Order orderC1 = new Order( new Customer("C1"), OperationType.SELL, ShareType.A,15,4);
+        Order orderC2 = new Order( new Customer("C2"), OperationType.BUY,  ShareType.C,15,4);
 
         assertFalse(orderC1.isCorresponds(orderC2));
     }
 
     @Test
     public void isNotCorrespondsDifferentPrice() throws Exception {
-        Order orderC1 = new Order( new Client("C1"), OperationType.SELL, ShareType.A,15,4);
-        Order orderC2 = new Order( new Client("C2"), OperationType.BUY,  ShareType.A,14,4);
+        Order orderC1 = new Order( new Customer("C1"), OperationType.SELL, ShareType.A,15,4);
+        Order orderC2 = new Order( new Customer("C2"), OperationType.BUY,  ShareType.A,14,4);
 
         assertFalse(orderC1.isCorresponds(orderC2));
     }
 
     @Test
     public void isNotCorrespondsDifferentCount() throws Exception {
-        Order orderC1 = new Order( new Client("C1"), OperationType.SELL, ShareType.A,15,4);
-        Order orderC2 = new Order( new Client("C2"), OperationType.BUY,  ShareType.A,15,0);
+        Order orderC1 = new Order( new Customer("C1"), OperationType.SELL, ShareType.A,15,4);
+        Order orderC2 = new Order( new Customer("C2"), OperationType.BUY,  ShareType.A,15,0);
 
         assertFalse(orderC1.isCorresponds(orderC2));
     }
