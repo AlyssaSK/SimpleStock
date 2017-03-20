0a3e2917f6bb40577a652eb457ebae6cb647ac2c
chervotkinaaa <ayman.00@mail.ru>
2017-03-20 11:40:31 +0300

    *  moved method to write to file + added comments;

--- src/main/java/com/simple/stock/service/FileService.java
+++ src/main/java/com/simple/stock/service/FileService.java
@@ -1,9 +1,16 @@
 package com.simple.stock.service;
 
+import com.simple.stock.model.Account;
+
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Collection;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class FileService {
@@ -12,6 +19,11 @@ public class FileService {
     public static final String CLIENTS_FILE_NAME = PATH + "clients.txt";
     public static final String RESULT_FILE_NAME  = PATH + "results.txt";
 
+    /**
+     * Возвращает поток строк полученный из файла
+     * @param pathToFile - расположение файла
+     * @return Stream<String>
+     */
     public static Stream<String> getStream(String pathToFile ) {
         Stream<String> stringStream = Stream.empty();
         try {
@@ -20,5 +32,27 @@ public class FileService {
             e.printStackTrace();
         }
         return stringStream;
+    }
+
+    /**
+     * Записывает коллекцию счетов в файл
+     * @param accounts  набор счетов
+     * @param path      расположение целевого файла
+     */
+    public static void writeAccounts(Collection<Account> accounts, String path) {
+        accounts = accounts.stream().sorted().collect(Collectors.toList());
+        try( PrintWriter pw = new PrintWriter( path ) ) {
+            AtomicInteger limit = new AtomicInteger(accounts.size()) ;
+            accounts.forEach(
+                    account -> {
+                        pw.print( account );
+
+                        if( limit.decrementAndGet() != 0 )
+                            pw.append( "\n" );
+                    });
+            pw.close();
+        } catch (FileNotFoundException e) {
+            System.err.println(e.getMessage());
+        }
     }
 }
