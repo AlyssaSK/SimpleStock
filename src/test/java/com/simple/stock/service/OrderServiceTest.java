4f6a332a0adbbc047eada788c7ffd1e487e0cf1d
chervotkinaaa <ayman.00@mail.ru>
2017-03-20 15:24:03 +0300

    + added test (processOrders)

--- src/test/java/com/simple/stock/service/OrderServiceTest.java
+++ src/test/java/com/simple/stock/service/OrderServiceTest.java
@@ -1,7 +1,10 @@
 package com.simple.stock.service;
 
+import com.simple.stock.model.Account;
+import com.simple.stock.model.Customer;
 import com.simple.stock.model.Order;
 import com.simple.stock.ref.OperationType;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.*;
@@ -36,9 +39,52 @@ public class OrderServiceTest {
         assertFalse( orderMap.get(OperationType.SELL).contains(expBuy1) );
     }
 
-    @Test
-    public void processOrders() throws Exception {
+    /*
 
+*/
+    private String[] strings = new String[4];
+    private Map<Customer, Account> accounts;
+    private Map<Customer, Account> accountsExpected;
+
+    private Map<OperationType, List<Order>> orders;
+    @Before
+    public void setUp() {
+        setCustomerAccountMap();
+        setExpectedCustomerAccountMap();
+
+        Stream<String> stringStream =
+                Stream.of(  "C1\ts\tC\t15\t5", // Обработается C1 +75$ -5C
+                            "C2\tb\tC\t15\t5", // Обработается C2 -75$ +5C
+                            "C2\ts\tC\t13\t2",
+                            "C4\tb\tD\t5\t4",
+                            "C4\ts\tD\t3\t2");
+        orders = OrderService.getOrderMap(stringStream);
     }
 
+    private void setCustomerAccountMap() {
+        this.accounts = new HashMap<Customer, Account>();
+        this.accounts.put( new Customer("C1"), Account.getAccountFromString("C1\t1000\t130\t240\t760\t320"));
+        this.accounts.put( new Customer("C2"), Account.getAccountFromString("C2\t4350\t370\t120\t950\t560"));
+        this.accounts.put( new Customer("C3"), Account.getAccountFromString("C3\t2760\t0\t0\t0\t0"));
+        this.accounts.put( new Customer("C4"), Account.getAccountFromString("C4\t560\t450\t540\t480\t950"));
+        this.accounts.put( new Customer("C5"), Account.getAccountFromString("C5\t1500\t0\t0\t400\t100"));
+    }
+
+    private void setExpectedCustomerAccountMap() {
+        // Обработаются две заявки:
+        // "C1\ts\tC\t15\t5"
+        // "C2\tb\tC\t15\t5"
+        this.accountsExpected = new HashMap<Customer, Account>();
+        this.accountsExpected.put( new Customer("C1"), Account.getAccountFromString("C1\t1075\t130\t240\t755\t320"));
+        this.accountsExpected.put( new Customer("C2"), Account.getAccountFromString("C2\t4275\t370\t120\t955\t560"));
+
+        this.accountsExpected.put( new Customer("C3"), Account.getAccountFromString("C3\t2760\t0\t0\t0\t0"));
+        this.accountsExpected.put( new Customer("C4"), Account.getAccountFromString("C4\t560\t450\t540\t480\t950"));
+        this.accountsExpected.put( new Customer("C5"), Account.getAccountFromString("C5\t1500\t0\t0\t400\t100"));
+    }
+
+    @Test
+    public void processOrders() throws Exception {
+        assertEquals( accountsExpected, OrderService.processOrders(accounts, orders));
+    }
 }
