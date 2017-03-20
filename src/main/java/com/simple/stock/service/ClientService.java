83e3e3720ce6045c933847e4e62f7dbc1ec5c5f5
chervotkinaaa <ayman.00@mail.ru>
2017-03-20 11:42:12 +0300

    *  moved method to write to file (into FileService); *  organized imports

--- src/main/java/com/simple/stock/service/ClientService.java
+++ src/main/java/com/simple/stock/service/ClientService.java
@@ -3,11 +3,7 @@ package com.simple.stock.service;
 import com.simple.stock.model.Account;
 import com.simple.stock.model.Client;
 
-import java.io.FileNotFoundException;
-import java.io.PrintWriter;
 import java.util.*;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class ClientService {
@@ -18,22 +14,5 @@ public class ClientService {
             if( account != null ) clientAccountMap.put( account.getKey(), account );
         } );
         return clientAccountMap;
-    }
-
-    public static void writeAccounts(Collection<Account> accounts, String path) {
-        accounts = accounts.stream().sorted().collect(Collectors.toList());
-        try(PrintWriter pw = new PrintWriter( path )) {
-            AtomicInteger limit = new AtomicInteger(accounts.size()) ;
-            accounts.forEach(
-                    account -> {
-                        pw.print( account );
-
-                        if( limit.decrementAndGet() != 0 )
-                            pw.append( "\n" );
-                    });
-            pw.close();
-        } catch (FileNotFoundException e) {
-            System.err.println(e.getMessage());
-        }
     }
 }
